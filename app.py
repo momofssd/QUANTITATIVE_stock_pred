904af9ec7c49a4cf16c36b41bebb0af32a774e54
ybm20323@gmail.com <139839439+jaym9958@users.noreply.github.com>
2026-05-04 23:23:59 -0500

    0504

--- app.py
+++ app.py
@@ -746,4 +746,5 @@ def serve_output(filename):
     return send_from_directory('outputs', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
