f593d955ad4dda7cadabd9a39a7384df98eea836
ybm20323@gmail.com <139839439+jaym9958@users.noreply.github.com>
2026-05-06 18:43:48 -0500

    0506

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useMemo, useState } from "react";
 import "./App.css";
 
-const API_BASE = import.meta.env.DEV ? "http://127.0.0.1:5000" : "";
+const API_BASE = "";
 
 function formatMoney(value) {
   return new Intl.NumberFormat("en-US", {
